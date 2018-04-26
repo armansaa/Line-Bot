c4ac8b1de6b43bc1d51442d6e817aece92a31c50
armansaa <armansa95@gmail.com>
2018-04-26 21:16:10 +0700

    fix

--- app.py
+++ app.py
@@ -37,10 +37,10 @@ app = Flask(__name__)
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 if channel_secret is None:
-    print 'Specify LINE_CHANNEL_SECRET as environment variable.'
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
 if channel_access_token is None:
-    print 'Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.'
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
     sys.exit(1)
 
 line_bot_api = LineBotApi(channel_access_token)
